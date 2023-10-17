e3954a276cb55129d19e48a3918aad70ba2cdd9f
Sanja Misic <sanjamisic98@gmail.com>
2023-10-17 14:27:41 +0200

    added error message depend on input

--- script.js
+++ script.js
@@ -11,5 +11,10 @@ accessBtn.addEventListener('click', function (event) {
 
   if (input.value === '' || !input.value.match(emailFormat)) {
     errorMessage.classList.remove('access__form-erorr-message--hidden');
+    if (input.value === '') {
+      errorMessage.innerText = 'Oops! Please add your email';
+    } else if (!input.value.match(emailFormat)) {
+      errorMessage.innerText = 'Oops! Please check your email';
+    }
   } else errorMessage.classList.add('access__form-erorr-message--hidden');
 });
